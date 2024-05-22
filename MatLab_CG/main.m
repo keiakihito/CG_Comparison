ff9c79d5dd2471e49c7a1ef268e39db58ef4b9c9
keiakihito <kkatsumi.git@gmail.com>
2024-05-21 20:58:10 -0700

    Comparing pcg() and my_pcg() with 10000 size SPD matrix

--- MatLab_CG/main.m
+++ MatLab_CG/main.m
@@ -1,13 +1,5 @@
-%%Create a random 3 X 3 matrix S
-%S = rand(3, 3);
-%disp('Matrix S');
-%disp(S);
-%
-%% Create SPD matrix with A^{T}*A
-%A = S' * S;
-%disp('SPD: A');
-%disp(A);
 
+% Manual Conjugeta Gradient
 function[x_sol] = my_pcg(A, b, tol, maxitr, x_0);
     %Set up return value, x_sol
     x_sol = x_0;
@@ -76,31 +68,86 @@ function[x_sol] = my_pcg(A, b, tol, maxitr, x_0);
 
 end % end of function
 
+%Error checking
+function[] = validate(x_ans, x_myPcg)
+    % Initialize maximum error
+    max_error = 0;
+
+    % Calculate the absolute error
+    error_vector = abs(x_ans - x_myPcg);
+
+    % Check and display if error exceeds previous maximum
+    for i = 1:length(error_vector)
+        if error_vector(i) > max_error
+            max_error = error_vector(i);
+            fprintf('Current max error at index %d: %e\n', i, max_error);
+        end % end of if
+    end % end of for
+
+    % Display the final maximum error
+    fprintf('Max error: %e\n', max_error);
+end % end of validate
+
+
+
+
 
 %Main function
+%Create a random 3 X 3 matrix S
+
+% ~~Test 1~~~
 %Set up 3 x 3 SPD matrix hardcoded
-fprintf('~~3 x 3 SPD matrix~~ \n');
-A = [1.5004 1.3293 0.8439; 1.3293 1.2436 0.6936; 0.8439 0.6936 1.2935];
-disp(A)
+%fprintf('~~3 x 3 SPD matrix~~ \n');
+%A = [1.5004 1.3293 0.8439; 1.3293 1.2436 0.6936; 0.8439 0.6936 1.2935];
+%disp(A)
 
-% Set given vector b = [1, 1, 1]
-b = [1; 1; 1];
+%% Set given vector b = [1, 1, 1]
+%b = [1; 1; 1];
 
-% Set up initial guess x_0 = [0, 0, 0]
-x_ans = [1000; 1000; 1000];
-x_0 = [1000; 1000; 1000];
+%% Set up initial guess x_0 = [0, 0, 0]
+%x_ans = [0; 0; 0];
+%x_0 = [0; 0; 0];
+
+
+
+
+% ~~Test 2~~~
+%Let N is row and column number of matrix
+N = 10000;
+S = rand(N, N);
+%disp('Matrix S');
+%disp(S);
+
+% Create SPD matrix with A^{T}*A
+A = S' * S;
+%disp('SPD: A');
+%disp(A);
+
+b = ones(N, 1);
+x_ans = zeros(N,1);
+x_0 = zeros(N,1);
+
+
+
 % Set epsilon
 eps = 1e-6;
 
 %Maxnumber of iteration
-maxItr = 1000;
+maxItr = 100000;
 
 %Answer key
 %Solve Ax = b with pcg
 fprintf('Slove Ax = b with pcg()\n');
 x_ans = pcg(A, b, eps, maxItr);
-disp(x_ans);
+%disp(x_ans);
 
+%Solve Ax = b with manual CG implemenation
 fprintf('Slove Ax = b with my_pcg()\n');
 x_myPcg = my_pcg(A, b, eps, maxItr, x_0);
-disp(x_myPcg)
+%disp(x_myPcg)
+
+%Compare answer and my solution
+validate(x_ans, x_myPcg);
+
+
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Conjugate Gradient Comparison
+There are three projects
+- CUDA
+- MATLAB
+- Python
+
+Trying to implement CG solver for Ax = b with given Pseudocode for NVIDIA GPU system. Comparing CUDA implementation with MATLAB and Python projects ensures my CUDA implementation is accurate.
+<br><br>
+
